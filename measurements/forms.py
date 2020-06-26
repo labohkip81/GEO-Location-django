62dfa10592b731cbee91d600068cff0bc517b2ca
Laban <labankiplagat81@gmail.com>
2020-06-26 13:58:25 +0300

    Measurements form and model.

---
+++ measurements/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Measurement
+
+
+class MeasurementModelForm(forms.ModelForm):
+    class Meta:
+        model = Measurement
+        fields = ('destination',)
+        
--- measurements/views.py
+++ measurements/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Measurement
 
 # Create your views here.
+
+def calculate_distance_view(request):
+    obj = get_object_or_404(Measurement, id=1)
+
+    context = {
+        'distance': obj,
+    }
+
+    return render(request, 'measurements/main.html', context)
