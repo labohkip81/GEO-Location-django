bc494228667e88caa255068bf1c1041753146cf4
Laban <labankiplagat81@gmail.com>
2020-06-27 10:32:09 +0300

    add folium package to stack.

--- measurements/views.py
+++ measurements/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Measurement
-from .forms import MeasurementModelForm
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
 
+import folium
+
+from .models import Measurement
+from .forms import MeasurementModelForm
 from .utils import get_geo
 
 # Create your views here.
@@ -17,11 +19,6 @@ def calculate_distance_view(request):
 
     country, city, lat, lon = get_geo(ip)
 
-
-    #print('location country =', country)
-    #print('location city =', city)
-    #print('location latitude longitude =', lat, lon)
-
     location = geolocator.geocode(city)
 
     print(f'######, {location}')
@@ -33,21 +30,27 @@ def calculate_distance_view(request):
     pointA = (location_lat, location_lon)
    
 
+   #initial folium map modification.
+
 
     if form.is_valid():
         instance = form.save(commit=False)
         destination_ = form.cleaned_data.get('destination')
         destination = geolocator.geocode(destination_)
-       # print(destination)
+
+        #destination coordinates.
         d_lat = destination.latitude
         d_long = destination.longitude
         instance.location = location
-        #location of the second location.
         pointB = (d_lat, d_long)
-
+        
+        #calculate distance using geodesic
         distance = round(geodesic(pointA, pointB).km, 2)
         instance.distance = distance
 
+        # folium map modification
+
+        #Save to the database.
         instance.save()
 
 
